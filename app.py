ff3036fc6f3580960679135c013183ac21e39596
asxrm <159586278+asxrm@users.noreply.github.com>
2025-12-13 03:33:17 +0300

    FINAL

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app .route("/")
 def home():
-    return "<h1>Welcome to Asma Shammakh's App!!!</h1>"
+    return "<h1>Welcome to Asma Shammakh's App!!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=500)
